960ea5ce4ab4d46d6855373b152c91195f17b77f
LZorro <timothy.volpe@gmail.com>
2018-01-27 12:38:52 -0800

    prototype pass of signal input buttons

---
+++ Assets/Scripts/SignalInput.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignalInput : MonoBehaviour {
+
+    //private Controls.cameraview curcamera;
+    public Image signalOutput;
+
+	// Use this for initialization
+	void Start () {
+        //curcamera = GameObject.Find("MainController").GetComponent<Controls>().currentCamera;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.Find("MainController").GetComponent<Controls>().currentCamera == Controls.cameraview.starboard)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+                this.transform.Find("SignalButtonUp").GetComponent<Button>().onClick.Invoke();
+            if (Input.GetKeyDown(KeyCode.A))
+                this.transform.Find("SignalButtonLeft").GetComponent<Button>().onClick.Invoke();
+            if (Input.GetKeyDown(KeyCode.D))
+                this.transform.Find("SignalButtonRight").GetComponent<Button>().onClick.Invoke();
+            if (Input.GetKeyDown(KeyCode.S))
+                this.transform.Find("SignalButtonDown").GetComponent<Button>().onClick.Invoke();
+            if (Input.GetKeyDown(KeyCode.R))
+                this.transform.Find("SignalButtonSend").GetComponent<Button>().onClick.Invoke();
+        }	
+	}
+
+    public void signalUp()
+    {
+        Debug.Log("Up");
+        signalOutput.color = Color.yellow;
+    }
+
+    public void signalDown()
+    {
+        Debug.Log("Down");
+        signalOutput.color = Color.green;
+    }
+
+    public void signalLeft()
+    {
+        Debug.Log("Left");
+        signalOutput.color = Color.blue;
+    }
+
+    public void signalRight()
+    {
+        Debug.Log("Right");
+        signalOutput.color = Color.red;
+    }
+
+    public void signalSend()
+    {
+        Debug.Log("Send Message: ");
+    }
+
+}
--- Assets/Scripts/ViewSwitch.cs
+++ Assets/Scripts/ViewSwitch.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class ViewSwitch : MonoBehaviour {
 	
 	public Camera StarboardCamera;
+    public Canvas StarboardCanvas;
 	public Camera BowCamera;
+    public Canvas BowCanvas;
+
+    private Controls.cameraview curcamera;
 
 	public void ShowStarboardCamera() {
 		BowCamera.enabled = false;
+        BowCanvas.enabled = false;
 		StarboardCamera.enabled = true;
+        StarboardCanvas.enabled = true;
+        curcamera = Controls.cameraview.starboard;
 	}
     
 	public void ShowBowCamera() {
 		BowCamera.enabled = true;
+        BowCanvas.enabled = true;
 		StarboardCamera.enabled = false;
-	}
+        StarboardCanvas.enabled = false;
+        curcamera = Controls.cameraview.bow;
+    }
 
 	public void ChangeCamera()
 	{
@@ -27,9 +37,11 @@ public class ViewSwitch : MonoBehaviour {
 		{
 			ShowBowCamera();
 		}
-	}
+        GameObject.Find("MainController").GetComponent<Controls>().currentCamera = curcamera;
+    }
 
 	void Start () {
+        curcamera = GameObject.Find("MainController").GetComponent<Controls>().currentCamera;
 		ShowStarboardCamera();
 	}
 }
