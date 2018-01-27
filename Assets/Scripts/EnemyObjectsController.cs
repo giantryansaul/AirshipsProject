ccb0e55a7dec991581c144989085371724325b5d
Ryan Saul <giantryansaul@gmail.com>
2018-01-27 12:34:48 -0800

    basic floating enemies flying toward the ship

---
+++ Assets/Scripts/EnemyObjectsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyObjectsController : MonoBehaviour
+{
+	private float floatingEnemySpawnTimer;
+	private int numberOfFloatingEnemiesLaunched = 0;
+	public GameObject FloatingEnemyPrefab;	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		floatingEnemySpawnTimer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		floatingEnemySpawnTimer += Time.deltaTime;
+		Debug.Log("Time " + floatingEnemySpawnTimer);
+		if (floatingEnemySpawnTimer / 10 > numberOfFloatingEnemiesLaunched)
+		{
+			Debug.Log("number of enemies " + numberOfFloatingEnemiesLaunched);
+			Instantiate(FloatingEnemyPrefab);
+			numberOfFloatingEnemiesLaunched++;
+		}
+		
+	}
+}
