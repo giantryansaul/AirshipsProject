acdf83c239edffc97c5ae223a6f6247b83acf1a3
Ryan Saul <giantryansaul@gmail.com>
2018-01-27 10:43:19 -0800

    added change camera

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			gameObject.GetComponent<ViewSwitch>().ChangeCamera();			
+		}
+	}
+}
