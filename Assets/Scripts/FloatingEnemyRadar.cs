836e15e3726d457e4367e124cf04045cc19f9f8d
Ryan Saul <giantryansaul@gmail.com>
2018-01-28 12:15:18 -0800

    radar movement for ships working

--- Assets/Scripts/FloatingEnemyRadar.cs
+++ Assets/Scripts/FloatingEnemyRadar.cs
@@ -3,31 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Radar : MonoBehaviour
+public class FloatingEnemyRadar : MonoBehaviour
 {
 	private GameObject radarPanelObject;
 	public int maxViewDistance = 20;
 	private Vector2 radarDimensions;
 
 	public Positions positionEnum;
+	public string targetShipName;
+
+	private float ourShipPos;
+	private float friendlyShipPos;
+	private float offsetPos;
 
 	void Start()
 	{
 		radarPanelObject = GameObject.Find("RadarPanel");
 		radarDimensions = radarPanelObject.GetComponent<RectTransform>().sizeDelta;
-		Debug.Log(positionEnum);
+
+		ourShipPos = radarDimensions.x * 0.25f;
+		friendlyShipPos = -ourShipPos;
+		offsetPos = radarDimensions.x * 0.125f;
+		
+		transform.position = new Vector2(0, 40); // just to get it out of the way
 	}
 	
 	public void UpdateRadarPosition(Vector2 position)
 	{
 		if (position.y < maxViewDistance)
 		{
-			var vertPos = position.y / maxViewDistance;
-			var horPos = 0;
+			var horPos = 0.0f;
+			if (targetShipName == "OurShip")
+			{
+				horPos = ourShipPos;
+			}
+			else if (targetShipName == "FriendlyShip")
+			{
+				horPos = friendlyShipPos;
+			}
+			
 			if (positionEnum == Positions.Left)
-				horPos = -1;
+				horPos += -offsetPos;
 			else if (positionEnum == Positions.Right)
-				horPos = 1;
+				horPos += offsetPos;
+			
+			var vertPos = position.y / maxViewDistance;
 			
 			transform.localPosition = new Vector2(horPos, vertPos * radarDimensions.y);
 		}
---
+++ Assets/Scripts/ShipRadar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipRadar : MonoBehaviour
+{
+	public GameObject ship;
+	private Positions position;
+	public RectTransform radarPanelObject;
+	private Vector2 radarDimensions;
+
+	private float shipOffset;
+	private float positionOffset;
+	private float shipPosition;
+	
+	void Start ()
+	{
+		radarDimensions = radarPanelObject.sizeDelta;
+		shipOffset = radarDimensions.x * 0.25f;
+		if (ship.name == "FriendlyShip")
+			shipOffset = -shipOffset;
+		positionOffset = radarDimensions.x * 0.125f;
+	}
+	
+	void Update ()
+	{
+		position = ship.GetComponent<ShipMovement>().Position;
+
+		if (position == Positions.Right)
+			shipPosition = shipOffset + positionOffset;
+		else if (position == Positions.Left)
+			shipPosition = shipOffset - positionOffset;
+		else
+			shipPosition = shipOffset;
+			
+		transform.localPosition = new Vector2(shipPosition, 20);
+	}
+}
